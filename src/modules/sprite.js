40157600c00d5a7164981e54297639fe36f42ee2
jeffrey <jeffrey@infloww.com>
2025-11-22 21:51:15 +0800

    1

--- src/modules/sprite.js
+++ src/modules/sprite.js
@@ -3,39 +3,39 @@
  */
 
 import * as THREE from "three";
-import {
-  getBufferGeometry
-} from "../common/geometry";
-import {
-  getMaterial
-} from "../common/material";
-import { globalConfig } from '../globalConfig';
-import g_2k_sun from "../resources/2k_sun.jpeg";
+import { getBufferGeometry } from "../common/geometry";
+import { getMaterial } from "../common/material";
+import { globalConfig } from "../globalConfig";
 
 export function add(SCENE) {
-
   const vertices = [];
-  const spriteCount = globalConfig.spriteCount
-  for (let i = 0; i < spriteCount; i++) {
+  const colors = [];
+  const spriteCount = globalConfig.spriteCount;
 
-    const x = THREE.MathUtils.randFloatSpread(6000);
-    const y = THREE.MathUtils.randFloatSpread(6000);
-    const z = THREE.MathUtils.randFloatSpread(6000);
-    vertices.push(x, y, z);
+  for (let i = 0; i < spriteCount; i++) {
+    vertices.push(
+      THREE.MathUtils.randFloatSpread(6000),
+      THREE.MathUtils.randFloatSpread(6000),
+      THREE.MathUtils.randFloatSpread(6000)
+    );
+
+    const color = new THREE.Color(Math.random(), Math.random(), Math.random());
+    colors.push(color.r, color.g, color.b);
   }
 
   const geometry = getBufferGeometry();
-  geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+  geometry.setAttribute(
+    "position",
+    new THREE.Float32BufferAttribute(vertices, 3)
+  );
+  geometry.setAttribute("color", new THREE.Float32BufferAttribute(colors, 3));
 
-  const TextureLoader = new THREE.TextureLoader();
-  const texture = TextureLoader.load(g_2k_sun);
-  const material = getMaterial('Points', {
-    size: 10,    
-    // map: texture,
-    // color: 0xFFFFFF * Math.random(),
-    color: 0xFFFFFF
+  const material = getMaterial("Points", {
+    size: 10,
+    vertexColors: true,
   });
+
   const points = new THREE.Points(geometry, material);
-  points._name = '粒子';
+  points._name = "粒子";
   SCENE.add(points);
 }
